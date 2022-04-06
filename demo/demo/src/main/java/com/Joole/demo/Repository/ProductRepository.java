5bf4bd317188a3a75cd61ba5e85765f6ad5f9d2a
treeofsoda <33137542+treeofsoda@users.noreply.github.com>
2022-04-06 01:27:02 -0700

    Delete demo directory

--- demo/demo/src/main/java/com/Joole/demo/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Joole.demo.Repository;
-
-import com.Joole.demo.Entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository
-    extends CrudRepository<Product, Long> {
-}
